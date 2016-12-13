d4d21db315d0d0719ee8ac3014e6bdb4ddcafc7a
shota2016 <karibe_shota@yse-c.net>
2016-12-13 16:12:19 +0900

    Aloha.java added.

--- Aloha.java
+++ Aloha.java
@@ -1,12 +1,15 @@
 public class Aloha  {
 	public static void main (String[] args){
-		String msg = "args[0]";
-		for(int i = 0; i<20; i++){
-			if(i%3 == 0) {
-		           System.out.println("アロハ！");
-			} else {
-		           System.out.println(msg);
-		}
-	   }
-       } 
+		String mainMsg ="アロハ！";
+		String subMsg ="ALOHA!";
+		for(int i =0; i<20; i++){
+			if(i%3 ==0) {
+			   mainMsg = new java.util.Scanner(System.in).nextLine();
+			}else{
+			   subMsg = new java.util.Scanner(System.in).nextLine();
+	      }
+	System.out.println(mainMsg);
+	System.out.println(subMsg);
+	}
+    }
 }
