23306aa8de1324cb867008d5c2908710fbbc6fa2
shota2016 <karibe_shota@yse-c.net>
2016-12-13 14:59:26 +0900

    Aloha.java cd

--- Aloha.java
+++ Aloha.java
@@ -1,7 +1,11 @@
 public class Aloha  {
 	public static void main (String[] args){
 		for(int i = 0; i<20; i++){
-		System.out.println(i+"ALOHA!");
+			if(i%3 == 0) {
+		           System.out.println("アロハ！");
+			} else {
+		           System.out.println("ALOHA!");
 		}
-	}
+	   }
+       } 
 }
